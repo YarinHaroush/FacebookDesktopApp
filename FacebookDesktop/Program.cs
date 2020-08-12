7cc7f754937e7fb9c0e676b8e8914ff4377e182a
YarinHaroush <55875010+YarinHaroush@users.noreply.github.com>
2020-08-12 10:37:12 +0300

    Add files via upload

---
+++ FacebookDesktop/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace A20_Ex01
+{
+     // $G$ THE-001 (-20) your grade on diagrams document - 80. please see comments inside the document. 50% of your grade).
+     public static class Program
+    {
+        ////[MTAThread]
+        [STAThread]
+        public static void Main()
+        {                        
+            Application.EnableVisualStyles();
+            Application.Run(FormFactory.CreateForm(typeof(FormMain)));
+        }
+    }
+}
